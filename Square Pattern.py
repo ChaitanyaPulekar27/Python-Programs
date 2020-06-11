eae503f61ac8065c76d36cab8b77db69fbea1323
ChaitanyaPulekar27 <66125529+ChaitanyaPulekar27@users.noreply.github.com>
2020-06-11 21:06:23 +0530

    square pattern
    In this code we get the knowledge of designing different patters and shapes.

---
+++ Square Pattern.py
@@ -0,0 +1,24 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+
+def square():
+    
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+
+square()
+my_turtle.forward(100)
+square()
+my_turtle.forward(100)
+square()
+my_turtle.forward(100)
+square()
+my_turtle.forward(100)
+    
