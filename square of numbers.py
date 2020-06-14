c7c60c4ffd7099dbdca5d72034c74f66faee1b8d
ChaitanyaPulekar27 <66125529+ChaitanyaPulekar27@users.noreply.github.com>
2020-06-14 22:07:52 +0530

    Square of numbers
    In this code we can define the square of any number given by the user.

---
+++ square of numbers.py
@@ -0,0 +1,13 @@
+# square of numbers
+
+#number = input("enter the number")
+#number = int(number)
+#square_number = number**2
+#print (square_number)
+
+def square_number(number):
+    number**2
+
+print(square_number)
+
+
