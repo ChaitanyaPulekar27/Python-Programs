c2abcc7eaf0ce02dbc1b5212f15ee7cc20636bb2
ChaitanyaPulekar27 <66125529+ChaitanyaPulekar27@users.noreply.github.com>
2020-06-14 22:09:48 +0530

    Update square of numbers.py

--- square of numbers.py
+++ square of numbers.py
@@ -1,13 +1,10 @@
 # square of numbers
 
-#number = input("enter the number")
-#number = int(number)
-#square_number = number**2
-#print (square_number)
+number = input("enter the number")
+number = int(number)
+square_number = number**2
+print (square_number)
 
-def square_number(number):
-    number**2
 
-print(square_number)
 
 
