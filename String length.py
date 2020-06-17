dba33f856f6725c76addc49abb2db06e1fa7fbfa
ChaitanyaPulekar27 <66125529+ChaitanyaPulekar27@users.noreply.github.com>
2020-06-17 22:03:05 +0530

    String length
    In this code if we pass a string it will give the count of the string.

---
+++ String length.py
@@ -0,0 +1,8 @@
+string_length = input("enter something:")
+#print(string_length)
+count = 0
+for char in string_length:
+    if char in string_length:
+        count = count+1
+
+print("the length of string is",count)
